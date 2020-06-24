ab1694e997666a7bff792a3c16aaf12f16d6e404
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2020-06-24 21:09:39 +0900

    fix & optimize count_pair_and_triad().  implement mc playout.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::fmt::Write;
 use std::*;
 
+#[derive(Clone)]
 struct TileSet {
     tiles: [u8; 34],
 }
@@ -49,19 +50,19 @@ const WEIGHT_TRIAD: usize = 3;
 
 fn count_pair_and_triad(hand: &mut TileSet) -> usize {
     let n_simples = [
-        count_triad_simple(hand, 0),
-        count_triad_simple(hand, 1),
-        count_triad_simple(hand, 2),
+        count_triad_simple(hand, 0, 0),
+        count_triad_simple(hand, 1, 0),
+        count_triad_simple(hand, 2, 0),
     ];
     let n_honor = count_triad_honor(hand);
 
     let mut n_total = n_simples[0] + n_simples[1] + n_simples[2] + n_honor;
     for t in 0..3 {
-        let n_others = n_simples[(t + 1) % 3] + n_simples[(t + 2) % 3] + WEIGHT_PAIR;
+        let n_others = n_simples[(t + 1) % 3] + n_simples[(t + 2) % 3] + n_honor + WEIGHT_PAIR;
         for i in 0..9 {
             if hand.simple(t, i) >= 2 {
                 *hand.simple_mut(t, i) -= 2;
-                n_total = cmp::max(n_total, count_triad_simple(hand, t) + n_others);
+                n_total = cmp::max(n_total, count_triad_simple(hand, t, 0) + n_others);
                 *hand.simple_mut(t, i) += 2;
             }
         }
@@ -79,64 +80,95 @@ fn count_pair_and_triad(hand: &mut TileSet) -> usize {
     n_total
 }
 
-fn count_triad_simple(hand: &mut TileSet, t: usize) -> usize {
+fn count_triad_simple(hand: &mut TileSet, t: usize, i0: usize) -> usize {
     let mut n_total = 0;
-    for i in 0..7 {
+    for i in i0..7 {
         if hand.simple(t, i + 0) > 0 && hand.simple(t, i + 1) > 0 && hand.simple(t, i + 2) > 0 {
             *hand.simple_mut(t, i + 0) -= 1;
             *hand.simple_mut(t, i + 1) -= 1;
             *hand.simple_mut(t, i + 2) -= 1;
-            n_total = cmp::max(n_total, count_triad_simple(hand, t) + WEIGHT_TRIAD);
+            n_total = cmp::max(n_total, count_triad_simple(hand, t, i) + WEIGHT_TRIAD);
             *hand.simple_mut(t, i + 0) += 1;
             *hand.simple_mut(t, i + 1) += 1;
             *hand.simple_mut(t, i + 2) += 1;
         }
     }
-    for i in 0..9 {
+    for i in i0..9 {
         if hand.simple(t, i) >= 3 {
             *hand.simple_mut(t, i) -= 3;
-            n_total = cmp::max(n_total, count_triad_simple(hand, t) + WEIGHT_TRIAD);
+            n_total = cmp::max(n_total, count_triad_simple(hand, t, i) + WEIGHT_TRIAD);
             *hand.simple_mut(t, i) += 3;
         }
     }
     n_total
 }
 
-fn count_triad_honor(hand: &mut TileSet) -> usize {
+fn count_triad_honor(hand: &TileSet) -> usize {
     let mut n_total = 0;
     for i in 0..7 {
-        if hand.honor(i) >= 3 {
-            *hand.honor_mut(i) -= 3;
-            n_total = cmp::max(n_total, count_triad_honor(hand) + WEIGHT_TRIAD);
-            *hand.honor_mut(i) += 3;
-        }
+        n_total += WEIGHT_TRIAD * (hand.honor(i) as usize / 3);
     }
     n_total
 }
 
-fn discard_tile(hand: &mut TileSet, wall: &mut TileSet, depth: usize) -> (f64, Vec<usize>) {
+fn discard_tile(hand: &mut TileSet, wall: &mut TileSet, depth: usize) -> (f64, Vec<(usize, f64)>) {
+    let n_tiles = hand.count();
     let count = count_pair_and_triad(hand);
-    if count == hand.count() || depth == 0 {
+    if count == n_tiles || depth == 0 {
         return (count as f64, Vec::new());
     }
 
     let mut best_score = 0.0;
-    let mut best_tile = Vec::new();
+    let mut discards = Vec::with_capacity(n_tiles);
     for i in 0..34 {
         if hand.tile(i) > 0 {
             *hand.tile_mut(i) -= 1;
             let score = draw_tile(hand, wall, depth - 1);
             *hand.tile_mut(i) += 1;
-            if score >= best_score {
-                if score > best_score {
-                    best_score = score;
-                    best_tile.clear();
-                }
-                best_tile.push(i);
-            }
+            discards.push((i, score));
+            best_score = f64::max(best_score, score);
         }
     }
-    (best_score, best_tile)
+    (best_score, discards)
+}
+
+fn playout(hand: &TileSet, wall: &TileSet, n_samples: usize) -> f64 {
+    let n_tiles = hand.count() + 1;
+    let mut rng = rand::thread_rng();
+    let mut acc = Vec::new();
+    for i in 0..34 {
+        for _ in 0..wall.tile(i) {
+            acc.push(i);
+        }
+    }
+    let mut sum = 0;
+    for _ in 0..n_samples {
+        let mut hand = hand.clone();
+        let mut acc = acc.clone();
+        for turn in 0.. {
+            let count = count_pair_and_triad(&mut hand);
+            if count >= n_tiles && count % 3 == 2 {
+                sum += turn;
+                break;
+            }
+            let tile = acc.remove(rand::Rng::gen_range(&mut rng, 0, acc.len()));
+            *hand.tile_mut(tile) += 1;
+        }
+    }
+    sum as f64 / n_samples as f64
+}
+
+fn discard_tile_by_mc(hand: &mut TileSet, wall: &TileSet, n_samples: usize) -> Vec<(usize, f64)> {
+    let mut discards = Vec::new();
+    for i in 0..34 {
+        if hand.tile(i) > 0 {
+            *hand.tile_mut(i) -= 1;
+            let score = playout(&hand, &wall, n_samples);
+            *hand.tile_mut(i) += 1;
+            discards.push((i, score));
+        }
+    }
+    discards
 }
 
 fn draw_tile(hand: &mut TileSet, wall: &mut TileSet, depth: usize) -> f64 {
@@ -229,57 +261,63 @@ fn parse_tile_set(text: &str) -> Option<TileSet> {
     Some(hand)
 }
 
-fn main() {
-    for arg in env::args().skip(1) {
-        let hand = parse_tile_set(&arg);
-        let mut hand = match hand {
-            Some(t) => t,
-            None => {
-                println!("Syntax error: {}", arg);
-                println!();
-                continue;
-            }
-        };
-
-        println!("Hand: {}", format_tile_set(&mut hand));
-        let mut wall = TileSet::new();
-        for i in 0..34 {
-            *wall.tile_mut(i) = 4 - hand.tile(i);
-        }
-        for i in 0..4 {
-            let (score, tiles) = discard_tile(&mut hand, &mut wall, i);
-            print!("depth = {} | score = {:.2} |", i, score);
-            for tile in tiles {
-                print!(" {}", format_tile(tile));
-            }
-            println!();
-        }
-        println!();
-    }
-}
-
-/*
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn generate_random_hand() -> TileSet {
     let mut rng = rand::thread_rng();
+    let mut hand = TileSet::new();
+    for _ in 0..14 {
+        *hand.tile_mut(rand::Rng::gen_range(&mut rng, 0, 34)) += 1;
+    }
+    hand
+}
 
-    loop {
-        let mut hand = TileSet::new();
-        for _ in 0..14 {
-            *hand.tile_mut(rand::Rng::gen_range(&mut rng, 0, 27)) += 1;
-        }
+fn analyze_hand(hand: &mut TileSet) {
+    println!("Hand: {}", format_tile_set(hand));
 
-        println!("Hand: {}", format_tile_set(&mut hand));
-        let mut wall = TileSet::new();
-        for i in 0..34 {
-            *wall.tile_mut(i) = 4 - hand.tile(i);
+    let mut wall = TileSet::new();
+    for i in 0..34 {
+        *wall.tile_mut(i) = 4 - hand.tile(i);
+    }
+
+    let (score, _) = discard_tile(hand, &mut wall, 0);
+    println!("  max-mean search, depth 0:");
+    println!("       {:>11.8}", score);
+    for i in 1..4 {
+        let (_, mut discards) = discard_tile(hand, &mut wall, i);
+        discards.sort_by(|(_, s0), (_, s1)| s1.partial_cmp(s0).unwrap());
+        println!("  max-mean search, depth {}:", i);
+        for (tile, score) in discards {
+            println!("    {} {:>11.8}", format_tile(tile), score);
         }
-        for i in 0..3 {
-            print!("Depth {} result:", i);
-            for tile in put(&mut hand, &mut wall, i).1 {
-                print!(" {}", format_tile(tile));
-            }
-            println!();
+    }
+
+    let n_samples = 65536;
+    let mut discards = discard_tile_by_mc(hand, &wall, n_samples);
+    discards.sort_by(|(_, s0), (_, s1)| s0.partial_cmp(s1).unwrap());
+    println!("  mc playout, {} samples:", n_samples);
+    for (tile, score) in discards.iter() {
+        println!("    {} {:>7.4}", format_tile(*tile), score);
+    }
+
+    println!();
+}
+
+fn main() {
+    if env::args().len() <= 1 {
+        loop {
+            let mut hand = generate_random_hand();
+            analyze_hand(&mut hand);
+        }
+    } else {
+        for arg in env::args().skip(1) {
+            let mut hand = match parse_tile_set(&arg) {
+                Some(t) => t,
+                None => {
+                    println!("Syntax error: {}", arg);
+                    println!();
+                    continue;
+                }
+            };
+            analyze_hand(&mut hand);
         }
     }
 }
-*/
